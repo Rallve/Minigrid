822cc895f560fd7ffbd164ca031315180b112d52
Rallve <lucas.liedgren@gmail.com>
2026-05-14 19:08:07 +0200

    Fixed some bugs, implemented probability and renamed the class

--- minigrid/envs/environment2.py
+++ minigrid/envs/environment2.py
@@ -1,29 +1,29 @@
 from __future__ import annotations
+import math
 from operator import add
+import random
 
 from minigrid.core.constants import COLOR_NAMES
 from minigrid.core.grid import Grid
 from minigrid.core.mission import MissionSpace
-from minigrid.core.world_object import Door, Goal, Key, Lava, Wall, SpikeFloor
+from minigrid.core.world_object import Door, Goal, Key, Lava, Point, Wall, WorldObj
 from minigrid.manual_control import ManualControl
 from minigrid.minigrid_env import MiniGridEnv
 
-from minigrid.wrappers import DeadlySpikes
 
-# Two potential issues:
-# 1. Obstacles may avoid the agent
-# 2. Walking into a wall may terminate the episode
-class Environment2(MiniGridEnv):
+class WanderingGuards(MiniGridEnv):
     def __init__(
         self,
         size=9,
         agent_start_pos=(1, 7),
         agent_start_dir=0,
+        prob=0.5,
         max_steps: int | None = None,
         **kwargs,
     ):
         self.agent_start_pos = agent_start_pos
         self.agent_start_dir = agent_start_dir
+        self.prob = prob
 
         mission_space = MissionSpace(mission_func=self._gen_mission)
 
@@ -78,42 +78,83 @@ class Environment2(MiniGridEnv):
 
         self.mission = "grand mission"
 
+    def _place_obj(
+        self,
+        obj: WorldObj | None,
+        top: Point = None,
+        size: tuple[int, int] = None,
+        reject_fn=None,
+        max_tries=math.inf,
+    ):
+        top = (max(top[0], 0), max(top[1], 0))
+
+        num_tries = 0
+
+        while True:
+            # This is to handle with rare cases where rejection sampling
+            # gets stuck in an infinite loop
+            if num_tries > max_tries:
+                raise RecursionError("rejection sampling failed in place_obj")
+
+            num_tries += 1
+
+            pos = (
+                self._rand_int(top[0], min(top[0] + size[0], self.grid.width)),
+                self._rand_int(top[1], min(top[1] + size[1], self.grid.height)),
+            )
+
+            # Don't place the object on top of another object
+            if self.grid.get(*pos) is not None:
+                continue
+
+            # Check if there is a filtering criterion
+            if reject_fn and reject_fn(self, pos):
+                continue
+
+            break
+
+        self.grid.set(pos[0], pos[1], obj)
+
+        if obj is not None:
+            obj.init_pos = pos
+            obj.cur_pos = pos
+
+        return pos
+
     def step(self, action):
         # Invalid action
         if action >= self.action_space.n:
             action = 0
 
-        # Check if there is an obstacle in front of the agent
-        front_cell = self.grid.get(*self.front_pos)
-        not_clear = front_cell and (front_cell.type != "goal" and front_cell.type != "key" and front_cell.type != "door")
-
         # Update obstacle positions
         for i_obst in range(len(self.obstacles)):
-            old_pos = self.obstacles[i_obst].cur_pos
-            top = tuple(map(add, old_pos, (-1, -1)))
 
-            try:
-                self.place_obj( # IMPORTANT: obstacles may avoid the agent! FIX THIS LATER
-                    self.obstacles[i_obst], top=top, size=(3, 3), max_tries=100
-                )
-                self.grid.set(old_pos[0], old_pos[1], None)
-            except Exception:
-                pass
+            if random.random() < self.prob:
+
+                old_pos = self.obstacles[i_obst].cur_pos
+                top = tuple(map(add, old_pos, (-1, -1)))
+
+                try:
+                    self._place_obj(
+                        self.obstacles[i_obst], top=top, size=(3, 3), max_tries=100
+                    )
+                    self.grid.set(old_pos[0], old_pos[1], None)
+                except Exception:
+                    pass
 
         # Update the agent's position/direction
         obs, reward, terminated, truncated, info = super().step(action)
 
-        # If the agent tried to walk over an obstacle or wall
-        if action == self.actions.forward and not_clear:
-            #reward = -1
+        # Terminate the episode if the agent touches lava
+        agent_cell = self.grid.get(*self.agent_pos)
+        if agent_cell and agent_cell.type == "lava":
             terminated = True
-            return obs, reward, terminated, truncated, info
 
         return obs, reward, terminated, truncated, info
 
 
 def main():
-    env = Environment2(render_mode="human")
+    env = WanderingGuards(render_mode="human", prob=1)
 
     # enable manual control for testing
     manual_control = ManualControl(env)
